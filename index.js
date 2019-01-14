0abaea09f5a51210c33bcfd82abf34bfec1dc7b7
Levindo Gabriel Taschetto Neto <levindogtn@gmail.com>
2019-01-14 14:25:03 -0200

    Add client builder

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-
-// var blip = require('balihoo-blip-sdk')
-// var blip2 = require('lime-transport-websocket')
+let BlipSdk = require('blip-sdk');
+let WebSocketTransport = require('lime-transport-websocket');
+let Lime = require('lime-js');
 var express = require('express');
 var config = require('config');
-var app = express();
+// var app = express();
+
+// Bot client
+var client = new BlipSdk.ClientBuilder()
+    .withIdentifier(config.get('blip.key'))
+    .withAccessKey(config.get('blip.key'))
+    .withTransportFactory(() => new WebSocketTransport())
+    .build();
 
 
-app.get('/', function (req, res) {
-  res.send('oi: ');
-})
+// app.get('/', function (req, res) {
+//   res.send('oi: ');
+// })
 
-app.listen(3000, () =>  {
-  console.log("Example app listening");
-  console.log("Port: ", config.get("server.port"));
-})
+// app.listen(3000, () =>  {
+//   console.log("Example app listening");
+//   console.log("Port: ", config.get("server.port"));
+// })
+
